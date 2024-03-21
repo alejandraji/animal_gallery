73e095e6e63fb333423346065e8ac0432ea91d21
alejandra <axjimenez89@gmail.com>
2024-03-20 23:05:25 -0400

    Move stuff from parent to child

--- client/src/AnimalFormModal.tsx
+++ client/src/AnimalFormModal.tsx
@@ -1,8 +1,9 @@
-import React, { FormEventHandler, MouseEventHandler } from 'react';
+import React, { FormEvent, FormEventHandler, MouseEventHandler } from 'react';
 import Modal from 'react-modal';
+import { postAnimal, putAnimal } from './api';
 import './AnimalFormModal.scss';
 
-const AnimalFormModal = ({ isOpen, closeModal, saveAnimal, currentAnimal, setCurrentAnimal }: { isOpen: boolean, closeModal: MouseEventHandler, saveAnimal: FormEventHandler, currentAnimal: any, setCurrentAnimal: any }) => {
+const AnimalFormModal = ({ isOpen, currentAnimal, setCurrentAnimal, setIsModalOpen, setSuccessMessage, setErrorMessage, addAnimal, replaceAnimal }: { isOpen: boolean, currentAnimal: any, setCurrentAnimal: any, setIsModalOpen: any, setSuccessMessage: any, setErrorMessage: any, addAnimal: any, replaceAnimal: any }) => {
   const customStyles = {
     content: {
       top: '50%',
@@ -21,6 +22,28 @@ const AnimalFormModal = ({ isOpen, closeModal, saveAnimal, currentAnimal, setCur
     })
   }
 
+
+  const onSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      if (currentAnimal.id === undefined) {
+        await postAnimal(currentAnimal);
+        setSuccessMessage("Item successfully added.");
+        addAnimal(currentAnimal);
+      } else {
+        await putAnimal(currentAnimal);
+        setSuccessMessage("Item successfully updated.");
+        replaceAnimal(currentAnimal)
+      }
+      setTimeout(() => setSuccessMessage(null), 3000);
+
+      setIsModalOpen(false);
+      setCurrentAnimal({name:'', description:''})
+    } catch (e: any) {
+      setErrorMessage(e.message)
+    }
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -28,7 +51,7 @@ const AnimalFormModal = ({ isOpen, closeModal, saveAnimal, currentAnimal, setCur
       style={customStyles}
     >
       <h2>Add animal</h2>
-      <form className="form-container" onSubmit={saveAnimal} method="post">
+      <form className="form-container" onSubmit={onSubmit} method="post">
         <div className="add-animal-form-input">
           <label htmlFor="name">Name:</label>
           <input
@@ -55,7 +78,7 @@ const AnimalFormModal = ({ isOpen, closeModal, saveAnimal, currentAnimal, setCur
         </div>
         <div className="form-btns">
           <button type="submit" className="add-btn">Save</button>
-          <button type="button" onClick={closeModal} className="cancel-btn">Cancel</button>
+          <button type="button" onClick={() => setIsModalOpen(false)} className="cancel-btn">Cancel</button>
         </div>
       </form>
     </Modal>
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,8 @@ import { postAnimal, getAnimals, putAnimal } from './api'
 import Animal from './Animal';
 import AnimalCard from './AnimalCard';
 
-const emptyAnimal = {name:'', description:''}
-
 const  App = () => {
-  const [currentAnimal, setCurrentAnimal] = useState<Animal>(emptyAnimal)
+  const [currentAnimal, setCurrentAnimal] = useState<Animal>({name:'', description:''})
   const [animals, setAnimals] = useState<Animal[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [successMessage,  setSuccessMessage] = useState<string | null>(null);
@@ -31,58 +29,21 @@ const  App = () => {
       .then(animals => setAnimals(animals))
   }, []);
 
-  /** Open the add animal modal */
-  const openModal = () => {
-    setIsModalOpen(true);
-  }
-
-  /** Close the add animal modal */
-  const closeModal = () => {
-    setIsModalOpen(false);
-  }
-
-  const saveAnimal = async (event: FormEvent) => {
-    event.preventDefault();
-    try {
-      if (currentAnimal.id === undefined) {
-        await postAnimal(currentAnimal);
-        setSuccessMessage("Item successfully added.");
-        addAnimal(currentAnimal);
-      } else {
-        await putAnimal(currentAnimal);
-        setSuccessMessage("Item successfully updated.");
-        replaceAnimal(currentAnimal)
-      }
-
-      setIsModalOpen(false);
-      setCurrentAnimal(emptyAnimal)
-    } catch (e: any) {
-      setErrorMessage(e.message)
-    }
-  }
-
-
-  const renderAnimalCards = () => {
-   return (
-    <>
-      {animals.length > 0 ?
-        (<ul className="animal-list-container">
-          {animals.map(animal =>
-            <AnimalCard
-              key={animal.id}
-              animal={animal}
-              removeAnimal={removeAnimal}
-              setIsModalOpen={setIsModalOpen}
-              setCurrentAnimal={setCurrentAnimal}
-            />
-          )}
-        </ul>)
-      :
-      <p>No animals. Try adding one.</p>
-      }
-    </>
-   )
-  }
+  const renderAnimalCards = () => animals.length > 0 ? (
+    <ul className="animal-list-container">
+      {animals.map(animal =>
+        <AnimalCard
+          key={animal.id}
+          animal={animal}
+          removeAnimal={removeAnimal}
+          setIsModalOpen={setIsModalOpen}
+          setCurrentAnimal={setCurrentAnimal}
+        />
+      )}
+    </ul>
+  ) : (
+    <p>No animals. Try adding one.</p>
+  );
 
   return (
     <main className="App">
@@ -91,15 +52,18 @@ const  App = () => {
         <div className="messages-btn">
           {successMessage && <p className="success">{successMessage}</p>}
           {errorMessage && <p className="error">{errorMessage}</p>}
-          <button onClick={openModal} className="create-btn">Add animal</button>
+          <button onClick={() => setIsModalOpen(true)} className="create-btn">Add animal</button>
         </div>
         {renderAnimalCards()}
         <AnimalFormModal
+          isOpen={isModalOpen}
           currentAnimal={currentAnimal}
           setCurrentAnimal={setCurrentAnimal}
-          isOpen={isModalOpen}
-          saveAnimal={saveAnimal}
-          closeModal={closeModal}
+          setIsModalOpen={setIsModalOpen}
+          setSuccessMessage={setSuccessMessage}
+          setErrorMessage={setErrorMessage}
+          addAnimal={addAnimal}
+          replaceAnimal={replaceAnimal}
         />
       </section>
     </main>
