13225ee18652ba3075bd0c5242312fdebef6ec63
alejandra <axjimenez89@gmail.com>
2024-03-20 21:28:15 -0400

    Delete comments/extra line

--- client/src/AnimalCards.tsx
+++ client/src/AnimalCards.tsx
@@ -4,26 +4,17 @@ import './AnimalCards.scss';
 import { deleteAnimalById } from './api';
 
 const AnimalCards = ({ animals, setAnimals } : { animals: Animal[], setAnimals: Dispatch<SetStateAction<Animal[]>> } ) => {
-  // const [animalList, setAnimalList] = useState<Animal[]>(animals);
-  // console.log("animalList", animalList)
-  console.log("animals props", animals)
-
-  // useEffect(() => {
-  //   console.log("Current animals prop:", animals);
-  //   setAnimalList(animals);
-  // }, [animals]);
-
   const deleteHandler = async (deleteId: number) =>{
     try {
       const response = await deleteAnimalById(deleteId);
-      setAnimals(animals.filter(animal => animal.id !== deleteId)); 
+      setAnimals(animals.filter(animal => animal.id !== deleteId));
       console.log('Animal deleted successfully');
     } catch (error) {
       console.error('Error on deleting animal', error);
     }
   }
 
-  const animalCardList = animals.map((animal: Animal) => ( 
+  const animalCardList = animals.map((animal: Animal) => (
     <li className="animal-tile" key={animal.id}>
         <a  href="" className="edit-btn" aria-label="delete">Edit</a>
       <button className="secondary-btn" onClick={() => deleteHandler(animal.id)} aria-label="delete">X</button>
